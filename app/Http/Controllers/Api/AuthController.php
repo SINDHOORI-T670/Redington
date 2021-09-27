997a1632febe3ac55f7168794dae700007b4ca50
Sindhoori T <73490459+SINDHOORI-T670@users.noreply.github.com>
2021-09-27 20:34:35 +0530

    api function updates

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,137 +1,295 @@
 <?php
 
+
+
 namespace App\Http\Controllers\Api;
 
+
+
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
+
 use App\User;
+
 use App\Models\UserSpec;
+
 use Illuminate\Support\Facades\Validator;
+
 use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Hash;
+
 use Illuminate\Support\Str;
+
 class AuthController extends Controller
+
 {
+
     public function login(Request $request){
+
         $validator = Validator::make($request->all(), [
+
             'phone' => 'required',
+
             'password' => 'required'
+
         ]);
+
         if($validator->fails()){
+
             $response['status']  = 'error';
+
             $response['message'] = $validator->messages()->first();
+
         }else{
+
             $credentials = $request->only('phone', 'password');
+
             if(Auth::attempt($credentials)){
+
                 $response['status']  = 'success';
+
                 $response['message'] = 'Logged in successfuly';
+
                 $response['token']   = Auth::user()->api_token;
+
                 $response['type'] = Auth::user()->type;
+
                 $response['user_id'] = Auth::user()->id;
+
             }else{
+
                 $response['status']  = 'error';
+
                 $response['message'] = 'Invalid credentials';
+
             }
+
         }
+
         return response()->json($response);
+
     }
+
+
 
     public function register(Request $request){
+
         $validator = $this->createValidation($request);
+
         if ($validator->fails()) {
+
             $response['status']  = 'error';
+
             $response['message'] = $validator->messages()->first();
+
         }else{
+
             DB::beginTransaction();
+
             $fileName = "";
+
             if ($request->file('image') != "") {
+
                 $file = $request->file('image');
+
                 $fileName = md5($file->getClientOriginalName() . time()) . "." . $file->getClientOriginalExtension();
+
                 $file->move('uploads/profiles/', $fileName);
+
                 $fileName = 'uploads/profiles/' . $fileName;
+
             }
+
             $userId = User::insertGetId([
+
                 'name' => $request->name,
+
                 'email' => $request->email,
+
                 'phone' => $request->phoneNumber,
+
                 'type'=>$request->type,
+
                 'status'=>0,
+
                 'verify_status'=>1,
+
                 'image'=>$fileName,
+
                 'password' => Hash::make($request->password),
+
                 'company'=> $request->company,
+
                 'post'=> $request->post,
+
                 'linkedin' => $request->linkedin,
+
                 'poc_id'=>$request->poc,
+
                 'api_token'=>Str::random(60),
+
                 "created_at" =>  \Carbon\Carbon::now(), # new \Datetime()
+
                 "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
 
+
+
             ]);
+
             $check=UserSpec::where('user_id',$userId)->get();
+
             $feature = UserSpec::insertGetId([
+
                 'user_id' => $userId,
+
                 'service_id' => isset($request->services)?implode(",",$request->services):null,
+
                 'technology_id' => isset($request->technologies)?implode(",",$request->technologies):null,
+
                 "created_at" =>  \Carbon\Carbon::now(), # new \Datetime()
+
                 "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
 
+
+
             ]);
+
             DB::commit();
+
             $user   = User::find($userId);
+
             $usertype = (
+
                 ($request->type == 2) ? "Customer" :
+
                 (($request->type == 3) ? "Partner" :
+
                 (($request->type == 4) ? "Employee" : "User"))
+
                 );
+                
+
                 $response['status']  = 'success';
+
                 $response['message'] = $usertype.' created successfuly';
+
+                $response['type'] = $user->type;
+
+                $response['user_id'] = $user->id;
+
                 $response['token']   = $user->api_token;
+
         }
+
         return response()->json($response);
+
     }
+
+
 
     public function createValidation($request)
+
     {
+
         if($request->type==4){
+
             $validator = Validator::make($request->all(),
+
             [
+
                 'name' => 'required',
+
                 'phoneNumber' => 'required|unique:users,phone',
+
                 'email' => 'required|email|max:255|regex:/(.*)@redington\.com/i|unique:users,email',
-                'poc' => 'required'
+
+                'poc' => 'required',
+                 
+                'password' => 'required',
+                'type' => 'required'
+
             ],[
+
                 'name.required' => 'Please enter name',
+
                 'phoneNumber.required' => 'Please enter phone number',
+
                 'email.required' => 'Please enter email',
+
                 'email.regex' => 'Domain not valid for registration(example@redington.com).',
-                'poc.required' => 'Please select type'
+
+                'poc.required' => 'Please select type',
+                'password.required'=>'Please enter password',
+                'type.required'=>'Please enter type'
+
             ]);
+
         }elseif($request->type==3){
+
             $validator = Validator::make($request->all(),
+
             [
+
                 'name' => 'required',
+
                 'phoneNumber' => 'required|unique:users,phone',
-                'email' => 'required|email|max:255|unique:users,email'
+
+                'email' => 'required|email|max:255|unique:users,email',
+                'password' => 'required',
+                'type' =>'required'
+
             ],[
+
                 'name.required' => 'Please enter name',
+
                 'phoneNumber.required' => 'Please enter phone number',
-                'email.required' => 'Please enter email'
+
+                'email.required' => 'Please enter email',
+
+                'password.required' => 'Please enter password',
+                'type.required'=>'Please enter type'
+
             ]);
+
         }else{
+
             $validator = Validator::make($request->all(),
+
             [
+
                 'name' => 'required',
+
                 'phoneNumber' => 'required|unique:users,phone',
-                'email' => 'required|email|max:255|unique:users,email'
+
+                'email' => 'required|email|max:255|unique:users,email',
+
+                'password' => 'required',
+
+                'type' => 'required'
+
             ],[
+
                 'name.required' => 'Please enter name',
+
                 'phoneNumber.required' => 'Please enter phone number',
-                'email.required' => 'Please enter email address'
+
+                'email.required' => 'Please enter email address',
+                'password.required' => 'Please enter password',
+                'type.required'=>'Please enter user type'
+
             ]);
+
         }
+
         return $validator;
+
     }
 
+
+
 }
+
--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -1,82 +1,158 @@
 <?php
 
+
+
 namespace App\Http\Controllers\Api;
 
+
+
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 use App\User;
+
 use App\Models\Service;
+
 use App\Models\Technology;
+
 use App\Models\Resource;
+
 use App\Models\SubResource;
+
 use App\Models\Journal;
+
 use App\Models\ValueJournal;
+
 use App\Models\ValueStory;
+
 use App\Models\Brand;
-use App\Models\MainService;
+
 use App\Models\Region;
+
 use App\Models\SalesConnect;
+
 use App\Models\PresetQuestion;
+
 use App\Models\Product;
-use App\Models\SubService;
+
+use App\Models\MainService;
+
 use App\Models\Events;
+
 class UserApiController extends Controller
+
 {
+
     public function api_validation($request){
+
         $tokenget = $request->header('Authorization');
+
         //return $tokenget;
+
         if(strpos($tokenget,' ') > -1)
+
             list($b,$t) = @explode(' ',$tokenget);
+
         else
+
             $b = $tokenget;
 
+
+
         if($b != '' && $b != "Bearer"){
+
             $data['response'] = "Token error!!";
+
             $data['status'] = "Error";
+
             //return response()->json($data);
+
             return 0;
+
         }
+
+
 
         $token = str_replace("Bearer ", "", $tokenget);
+
         $valid = $this->validateInternalToken($token,$request->user_id);
+
         return  $valid;
+
     }
+
+
 
     public function validateInternalToken($token,$userId){
+
         // $actualToken = config('constants.TOKEN');
+
         $user = User::where('id',$userId)->where('api_token',$token)->exists();
+
         if($user){
+
             return 1;
+
         }else{
+
             return 0;
+
         }  
+
     }
+
+
 
     public function operation_validation($request){
+
         if(!isset($request['op'])){
+
             return "Error";
+
         }
+
         else{
+
             return $request['op'];
+
         }
+
+
 
     }
 
+
+
     public function apiOperations(Request $request){
+
         $valid = $this->api_validation($request);
+
         if($valid != 1){
+
             $data['response'] = 'Invalid Token';
+
             $data['status'] = "Error";
+
             return response()->json($data);
+
         }
 
+
+
         $op = $this->operation_validation($request);
+
         if($op == "Error"){
+
             $data['response'] = 'No OP';
+
             $data['status'] = "Error";
+
             return response()->json($data);
+
         }
+
+
 
         switch ($op) {
 
@@ -145,6 +221,7 @@ class UserApiController extends Controller
                 $data=$this->products($request);
 
                 break;
+
             case 'main-services': 
                 $data=$this->mainserviceList($request);
                 break;
@@ -154,7 +231,7 @@ class UserApiController extends Controller
             case 'pastevents': 
                 $data=$this->pasteventsList($request);
                 break;
-
+    
             default:
 
                 $data['response'] = 'Invalid OP';
@@ -162,62 +239,116 @@ class UserApiController extends Controller
                 $data['status'] = "Error";
 
         }
+
         return response()->json($data);
+
     }
+
+
 
     public function getProfile(Request $request,$user_id){
         $userData   = User::with('userSpec')->where('id',$user_id)->first();
         return $userData;
     }
 
+
+
     public function serviceList(Request $request){
+
         $serviceData = Service::where('status',0)->get();
+
         return $serviceData;
+
     }
+
+
 
     public function technologyList(Request $request){
+
         $technologyData = Technology::where('status',0)->get();
+
         return $technologyData;
+
     }
+
+
 
     public function resourceList(Request $request){
-        $resourceData = Resource::where('status',0)->get();
-        return $resourceData;
+
+        $resourceData = Resource::where('status',0)->whereJsonContains('type', 2)->get();
+        $response['data']=$resourceData;
+        return $response;
+
     }
+
+
 
     public function subresourceList($id){
+
         $subresourceData = SubResource::where('resource_id',$id)->where('status',0)->get();
+
         return $subresourceData;
+
     }
+
+
 
     public function journalList(){
+
         $journalData = Journal::where('status',0)->get();
+
         return $journalData;
+
     }
+
+
 
     public function subJournals($id){
+
         $subJournalData = ValueJournal::where('journal_id',$id)->where('status',0)->get();
+
         return $subJournalData;
+
     }
+
     
+
     public function valuestories(){
+
         $storyData = ValueStory::where('status',0)->get();
+
         return $storyData;
+
     }
+
+
 
     public function brands(){
+
         $brandData = Brand::where('status',0)->get();
+
         return $brandData;
+
     }
+
+
 
     public function regions(){
+
         $regionData = Region::where('status',0)->get();
+
         return $regionData;
+
     }
 
+
+
     public function salesconnectList(){
+
         $salesData = SalesConnect::where('status',0)->get();
+
         return $salesData;
+
     }
 
     public function presetQuestions(){
@@ -232,15 +363,20 @@ class UserApiController extends Controller
 
     public function mainserviceList(Request $request){
         $serviceData = MainService::where('status',0)->get();
-        return $serviceData;
+        $response['data']= $serviceData;
+        return $response;
     }
 
     public function neweventsList(Request $request){
-        $eventData = Events::where('status',0)->get();
+        $now_str = \Carbon\Carbon::now();
+        $eventData = Events::where('date_time', '>=', $now_str)->where('status',0)->get();
         return $eventData;
     }
     public function pasteventsList(Request $request){
-        $eventData = Events::where('status',0)->get();
+        $now_str = \Carbon\Carbon::now();
+        $eventData = Events::where('date_time', '<', $now_str)->where('status',0)->get();
         return $eventData;
     }
+
 }
+
